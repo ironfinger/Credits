dd13aaf05ab2dd76662ff5b9d4f0d3868f952561
ironfinger <tomhoughton888@gmail.com>
2018-11-22 11:24:35 +0000

    Added a transactions endpoint and ability to have a wallet and transaction pool in node

--- app/index.js
+++ app/index.js
@@ -2,17 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../Blockchain');
 const P2pSerevr = require('./p2p-server');
+const Wallet = require('../Wallet');
+const TransactionPool = require('../Wallet/transaction-pool');
 
-const HTTP_PORT = process.env.HTTP_PORT ||  3001;
+const HTTP_PORT = process.env.HTTP_PORT ||  3020;
 
 const app = express();
 const bc = new Blockchain();
 const p2pServer = new P2pSerevr(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
+
+wallet.createTransaction('recipient', 20, tp);
 
 app.use(bodyParser.json());
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
 });
 
 app.post('/mine', (req, res) => {
